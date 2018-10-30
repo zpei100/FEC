2af5c2e5e8367b1544d47906e425d7a61c6276f9
zpei <zhengqing.pei@gmail.com>
2018-10-30 18:21:00 -0400

    added some comments

--- src/client/src/components/gallery/modal/Captions.js
+++ src/client/src/components/gallery/modal/Captions.js
@@ -32,18 +32,3 @@ const mapStateToProps = function(state) {
 }
 
 export default connect(mapStateToProps)(Captions);
-
-
-
-/*
-<div id="Captions" className="container-fluid">
-      <div style={{ width: `${carouselWidth}px`, overflow: 'hidden' }} className="m-auto">
-        <div id="caption-text">CAPTIONS TEXT AND STUFF</div>
-        <div id="caption-slides" className="d-flex flex-nowrap">
-          {imgs.map((img, idx) => (
-            <img key={idx} src={img} alt="" id={`caption-img-${idx}`} className="h-100 w-auto mx-1" />
-          ))}
-        </div>
-      </div>
-    </div>
-*/
--- src/client/src/components/gallery/modal/CarouselModal.js
+++ src/client/src/components/gallery/modal/CarouselModal.js
@@ -7,11 +7,10 @@ import ExitBtn from './ExitBtn';
 import Carousel from './Carousel';
 import Captions from './Captions';
 
-import changeActiveImage from "../../../actionCreators/changeActiveImage";
+import changeActiveImage from '../../../actionCreators/changeActiveImage';
 import { bindActionCreators } from 'redux';
 
 class CarouselModal extends Component {
-
   constructor(props) {
     super(props);
 
@@ -19,146 +18,182 @@ class CarouselModal extends Component {
       animating: false,
       animationDuration: 200,
       carouselWidth: window.innerWidth > 1036 ? 1036 : window.innerWidth
-    }
+    };
   }
 
-  handleCarouselTurn (type) {
+  //this function defines the logic of the modal-carousel. do not adjust this block of code or the slider will break.
 
+  //see a commet below for the end of the carousel logic.
+  handleCarouselTurn(type) {
     var carouselWidth = this.state.carouselWidth;
     var carouselHalfWidth = carouselWidth / 2;
     var captionImageWidth = $('#Captions ol img')[0].width;
-    var previousActiveImageOffSet = $('#Captions ol .active')[0].offsetLeft + captionImageWidth / 2;
+    var previousActiveImageOffSet =
+      $('#Captions ol .active')[0].offsetLeft + captionImageWidth / 2;
 
     var allCaptionImages = $('#Captions ol li');
-    var lastCaptionImageOffSet = allCaptionImages[allCaptionImages.length - 1].offsetLeft;
+    var lastCaptionImageOffSet =
+      allCaptionImages[allCaptionImages.length - 1].offsetLeft;
 
     var totalOffSet = $('#Captions ol')[0].offsetLeft;
 
     if (!this.state.animating) {
-
       var newActive;
-      if (this.props.activeImage === allCaptionImages.length - 1 && type === 'next') newActive = 0;
-      else if (this.props.activeImage === 0 && type === 'prev') newActive = allCaptionImages.length - 1;
-      else newActive = this.props.activeImage + (type === 'next' ? 1 : -1); 
+      if (
+        this.props.activeImage === allCaptionImages.length - 1 &&
+        type === 'next'
+      )
+        newActive = 0;
+      else if (this.props.activeImage === 0 && type === 'prev')
+        newActive = allCaptionImages.length - 1;
+      else newActive = this.props.activeImage + (type === 'next' ? 1 : -1);
 
       //use action creator
       this.props.changeActiveImage(newActive);
 
-      this.setState({
-        animating: true
-      }, () => {
-        
-        var currentActiveImageOffSet = $('#Captions ol .active')[0].offsetLeft + captionImageWidth / 2;
-        var move = 0;
+      this.setState(
+        {
+          animating: true
+        },
+        () => {
+          var currentActiveImageOffSet =
+            $('#Captions ol .active')[0].offsetLeft + captionImageWidth / 2;
+          var move = 0;
 
-        if (type === 'next') {
-          if (this.props.activeImage === 0) move = totalOffSet; 
-          
-          else if (currentActiveImageOffSet > carouselHalfWidth) {
-            if (previousActiveImageOffSet > carouselHalfWidth) {
-              move = currentActiveImageOffSet - previousActiveImageOffSet;
-              var lastImageDiff = lastCaptionImageOffSet  + totalOffSet - carouselWidth ;
-              if (lastImageDiff < 0) move += lastImageDiff;
-            }
-            else move = currentActiveImageOffSet - carouselHalfWidth
-          } 
-        }
-
-        if (type === 'prev') {
-          if (this.props.activeImage === allCaptionImages.length - 1) move = lastCaptionImageOffSet - carouselWidth + captionImageWidth;
-          else {
-            var distanceFromActiveToLast = lastCaptionImageOffSet + captionImageWidth - currentActiveImageOffSet;
-            if (distanceFromActiveToLast > carouselHalfWidth) {
-              if (distanceFromActiveToLast - carouselHalfWidth > captionImageWidth) {
+          if (type === 'next') {
+            if (this.props.activeImage === 0) move = totalOffSet;
+            else if (currentActiveImageOffSet > carouselHalfWidth) {
+              if (previousActiveImageOffSet > carouselHalfWidth) {
                 move = currentActiveImageOffSet - previousActiveImageOffSet;
-               
-                if (move < totalOffSet) move = totalOffSet;
-              }
-              else move = carouselHalfWidth - distanceFromActiveToLast;
+                var lastImageDiff =
+                  lastCaptionImageOffSet + totalOffSet - carouselWidth;
+                if (lastImageDiff < 0) move += lastImageDiff;
+              } else move = currentActiveImageOffSet - carouselHalfWidth;
             }
           }
+
+          if (type === 'prev') {
+            if (this.props.activeImage === allCaptionImages.length - 1)
+              move = lastCaptionImageOffSet - carouselWidth + captionImageWidth;
+            else {
+              var distanceFromActiveToLast =
+                lastCaptionImageOffSet +
+                captionImageWidth -
+                currentActiveImageOffSet;
+              if (distanceFromActiveToLast > carouselHalfWidth) {
+                if (
+                  distanceFromActiveToLast - carouselHalfWidth >
+                  captionImageWidth
+                ) {
+                  move = currentActiveImageOffSet - previousActiveImageOffSet;
+
+                  if (move < totalOffSet) move = totalOffSet;
+                } else move = carouselHalfWidth - distanceFromActiveToLast;
+              }
+            }
+          }
+
+          $('#Captions ol').animate({ marginLeft: `-=${move}px` }, 500);
+
+          var _this = this;
+          setTimeout(function() {
+            _this.setState({ animating: false });
+          }, _this.state.animationDuration);
         }
-      
-        $('#Captions ol').animate({marginLeft: `-=${move}px` }, 500);
-          
-        var _this = this;
-        setTimeout(function() {_this.setState({animating: false})}, _this.state.animationDuration)
-      })
+      );
     }
   }
 
-  toggleActiveCaption () {
+  toggleActiveCaption() {
     $($('.carousel-item')[this.props.activeImage]).toggleClass('active');
   }
 
-  handleModalShown () {
+  handleModalShown() {
     $('document').ready(() => {
       this.toggleActiveCaption();
 
       var carouselWidth = this.state.carouselWidth;
       var carouselHalfWidth = carouselWidth / 2;
       var captionImageWidth = $('#Captions ol img')[0].width;
-  
+
       var allCaptionImages = $('#Captions ol li');
-      var lastCaptionImageOffSet = allCaptionImages[allCaptionImages.length - 1].offsetLeft;
-  
+      var lastCaptionImageOffSet =
+        allCaptionImages[allCaptionImages.length - 1].offsetLeft;
 
-      var currentActiveImageOffSet = $('#Captions ol .active')[0].offsetLeft + captionImageWidth / 2;
+      var currentActiveImageOffSet =
+        $('#Captions ol .active')[0].offsetLeft + captionImageWidth / 2;
 
-      var move = 0
+      var move = 0;
       if (currentActiveImageOffSet > carouselHalfWidth) {
         move = currentActiveImageOffSet - carouselHalfWidth;
         var diff = lastCaptionImageOffSet + captionImageWidth - move;
-        if ( diff < carouselWidth) {
+        if (diff < carouselWidth) {
           move -= carouselWidth - diff;
-        } 
+        }
       }
 
-      $('#Captions ol').animate({marginLeft: `-=${move}px` }, 500);
-
-    })
+      $('#Captions ol').animate({ marginLeft: `-=${move}px` }, 500);
+    });
   }
 
   componentDidUpdate(prevProps) {
-    if (this.props.showModal !== prevProps.showModal && this.props.showModal === true) {
+    if (
+      this.props.showModal !== prevProps.showModal &&
+      this.props.showModal === true
+    ) {
       if (this.props.activeImg !== prevProps.activeImg) {
-
         // use action creator
       } else this.handleModalShown();
     }
   }
+  //end of carousel logic.
 
-  componentDidMount () {
+  componentDidMount() {
     $('document').ready(() => {
-      $('.carousel-fade .carousel-inner .carousel-item').css('transition-duration', `${this.state.animationDuration}ms`);  
+      $('.carousel-fade .carousel-inner .carousel-item').css(
+        'transition-duration',
+        `${this.state.animationDuration}ms`
+      );
       this.toggleActiveCaption();
       // $('.carousel-arrows').on('click', function(e) {
-        
-        //have not figured out a way to disable the arrow keys yet. If the fade animation of the slides take longer than 200ms, and the user clicks the arrows too quickly in succession, the shown slider and the caption image will not match;
 
-        // $(this).attr('disabled', true)
-        // console.log('show this')
-        // var _this = this;
-        // $(this).attr('disabled', true);
-        // console.log('arrows should be disabled: ', this)
-        // setTimeout(function() {
-        //   $(_this).removeAttr('disabled');
-        //   console.log('button is being enabled');
-        // }, component.state.animationDuration)
+      //have not figured out a way to disable the arrow keys yet. If the fade animation of the slides take longer than 200ms, and the user clicks the arrows too quickly in succession, the shown slider and the caption image will not match;
+
+      // $(this).attr('disabled', true)
+      // console.log('show this')
+      // var _this = this;
+      // $(this).attr('disabled', true);
+      // console.log('arrows should be disabled: ', this)
+      // setTimeout(function() {
+      //   $(_this).removeAttr('disabled');
+      //   console.log('button is being enabled');
+      // }, component.state.animationDuration)
       // })
-    })
+    });
   }
 
   render() {
-
-    var carouselWidth = this.state.carouselWidth >= 1036 ? 1036 : this.state.carouselWidth;
+    var carouselWidth =
+      this.state.carouselWidth >= 1036 ? 1036 : this.state.carouselWidth;
     const { handleCloseModal, imgs } = this.props;
 
     return (
-      <ReactModal isOpen={this.props.showModal} className="Modal" ariaHideApp={false}>
+      <ReactModal
+        isOpen={this.props.showModal}
+        className="Modal"
+        ariaHideApp={false}
+      >
         <ExitBtn />
-        <Carousel carouselWidth={carouselWidth} handleCarouselTurn={this.handleCarouselTurn.bind(this)} />
-        <Captions carouselWidth={carouselWidth} id="Captions" imgs={imgs} active={this.state.active}/>
+        <Carousel
+          carouselWidth={carouselWidth}
+          handleCarouselTurn={this.handleCarouselTurn.bind(this)}
+        />
+        <Captions
+          carouselWidth={carouselWidth}
+          id="Captions"
+          imgs={imgs}
+          active={this.state.active}
+        />
       </ReactModal>
     );
   }
@@ -168,15 +203,18 @@ const mapStateToProps = function(state) {
   return {
     showModal: state.showModal,
     activeImage: state.activeImage
-  }
-}
+  };
+};
 
 const mapDispatchToProps = function(dispatch) {
   return {
     changeActiveImage: bindActionCreators(changeActiveImage, dispatch)
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(CarouselModal);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CarouselModal);
 
 //<Captions carouselWidth={carouselWidth} id="Captions" imgs={imgs}/>
