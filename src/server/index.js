9310f27bde7c9b70e6d6a5e7cf98c95fc18bf947
zpei <zhengqing.pei@gmail.com>
2018-11-01 18:29:02 -0400

    fixed code to deploy

--- src/server/index.js
+++ src/server/index.js
@@ -151,6 +151,53 @@ app.get('/rooms/:id', function(req, res) {
 
 //routes to initialize csr client
 
+app.get('/getRoom/:id', function(req, res) {
+  const userId = req.session.user ? req.session.user : 15;
+
+  try {
+    const id = parseInt(req.params.id);
+    if (typeof id !== 'number') throw new Error('id invalid');
+
+    Room.find({ id }).then(rooms => {
+
+      let relatedListings = [];
+      const room = rooms[0];
+      const related = room.related;
+  
+      related.forEach((id, idx, related) => {
+        Room.find({ id }).then(rooms => {
+          relatedListings.push(rooms[0]);
+          if (relatedListings.length === related.length) {
+            User.find({ id: userId }).then(users => {
+              const { favorites, id, username } = users[0];
+              const user = {favorites, id, username }
+              const store = createStore(rootReducer, { room, relatedListings, user }, applyMiddleware(thunk));
+              const initialState = store.getState();
+              const content_one = renderToString(
+                <Provider store={store}>
+                  <Gallery />
+                </Provider>
+              );
+  
+              const content_two = renderToString(
+                <Provider store={store}>
+                  <RelatedListings />
+                </Provider>
+              );
+  
+              res.status(200).send({initialState, content_one, content_two});
+            }).catch(() => res.status(404));
+          }
+        }).catch(() => res.status(404));;
+      });
+    });
+  } catch(err) {
+    res.status(404)
+  }
+})
+
+
+
 app.get('/csr/getUser', function(req, res) {
   const userId = req.session.user ? req.session.user : 15;
 
